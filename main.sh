ce972deb80979473af1c21bad26a72011f95d24a
nek0zyx <nek0zyx@costmiku.space>
2025-06-21 22:22:56 +0200

    Initial commit?

---
+++ main.sh
@@ -0,0 +1,184 @@
+#!/bin/bash
+
+export ServerVersion
+export ServerRoot=$(dirname $0)
+source $ServerRoot/cpm.conf
+
+
+txtblk=$(tput setaf 0) # Black
+txtred=$(tput setaf 1) # Red
+txtgrn=$(tput setaf 2) # Green
+txtylw=$(tput setaf 3) # Yellow
+txtblu=$(tput setaf 4) # Blue
+txtpur=$(tput setaf 5) # Purple
+txtcyn=$(tput setaf 6) # Cyan
+txtwht=$(tput setaf 7) # White
+txtreg=$(tput sgr0)    # Normal
+
+
+function Main() {
+    command -v wget || LogFail "Command wget not found."
+    command -v less || LogFail "Command less not found."
+    case $1 in 
+        install)
+            InstallPackage $2 $3
+        ;;
+        remove|uninstall)
+            RemovePackage $2
+        ;;
+        init)
+            InitialiseChiselServer
+        ;;
+        info)
+            SelectedPackage=$2
+            GetPackageInformation
+        ;;
+        versions)
+            SelectedPackage=$2
+            GetPackageVersions
+        ;;
+        *)
+            LogFail "No such command as $1"
+    esac
+}
+
+function InitialiseChiselServer {
+    if [[ -f $ServerRoot/cpm.conf ]]; then
+        LogFail "Chisel is already here."
+        exit 1
+    fi
+    printf "ServerVersion=\n" > $ServerRoot/cpm.conf
+    echo "-- Modify cpm.conf and specify your server version."
+}
+
+function InstallPackage {
+    IsRunningInServer                    || LogFail "You are not running ChiselPM inside of a server that has the ChiselPM configuration file." 
+    SelectedPackage=$1
+    SelectedVersion=$2
+    if [[ $SelectedPackage == "" ]]; then
+        LogFail "Please specify a package."
+    fi
+
+    if [[ $SelectedVersion == "" ]]; then
+        LogFail "Please specify a package version. You can see the package versions with the \"versions\" subcommand."
+    fi
+
+    if [[ $(PackageExists $SelectedPackage) != "NONEXISTENT" ]]; then # If it is confirmed
+        LogFail "Package $SelectedPackage already exists"
+    fi
+    GET>/dev/null;
+    PackageWorksOnServer                 || LogFail "The package requested does not work on servers." 
+    PackageIsCompatibleWithServer        || LogFail "The package requested is not compatible with the server version."
+    PackageVersionIsCompatibleWithServer || LogFail "The version requested is not compatible with the server version."
+    Log i $SelectedPackage "Downloading package"
+    wget $(GetPackageFileURLGivenVersion) -qO $ServerRoot/mods/${SelectedPackage}_${SelectedVersion}.jar || LogFail "Failed either getting file, or writing it to the mods folder."
+    Log i $SelectedPackage "Package $SelectedPackage is now installed."
+}
+
+function RemovePackage {
+    if [[ $SelectedPackage == "" ]]; then
+        LogFail "Please specify a package."
+    fi
+    IsRunningInServer || LogFail "You are not running ChiselPM inside of a server that has the ChiselPM configuration file." 
+    SelectedPackage=$1
+    PackageExists $SelectedPackage || LogFail "Package $SelectedPackage does not exist."
+    echo "File(s) to be removed from $ServerRoot/mods/:
+        $(ls $ServerRoot/mods | grep $SelectedPackage_)"
+    read -pr "Are you sure you want to remove $SelectedPackage from the server? (y/n)" UninstallConfirmation
+    case $UninstallConfirmation in
+        "y"|"Y"|"yes"|"YES")
+            true
+        ;;
+        *)
+            LogFail "User denied"
+        ;;
+    esac
+    Log r $SelectedPackage "Deleting file"
+    rm -v "$ServerRoot"/mods/"${SelectedPackage}"_*.jar
+    Log r $SelectedPackage "File deleted."
+}
+
+function GET {
+    curl https://api.modrinth.com/v2/project/$SelectedPackage || LogFail "Failed to GET to server, either the package does not exist or a connection to the server couldn't be made."
+}
+
+function GetPackageFileURLGivenVersion {
+    curl https://api.modrinth.com/v2/project/$SelectedPackage/version/$SelectedVersion | jq -r ".files[].url"
+}
+
+function PackageWorksOnServer {
+    case $(curl https://api.modrinth.com/v2/project/$SelectedPackage | jq -r .server_side) in
+        "required"|"optional")
+            true
+        ;;
+        *)
+            false
+        ;;
+    esac
+}
+
+function PackageIsCompatibleWithServer {
+    curl https://api.modrinth.com/v2/project/$SelectedPackage | jq -r ".game_versions[]" | grep $ServerVersion
+}
+
+function PackageVersionIsCompatibleWithServer {
+    curl https://api.modrinth.com/v2/project/$SelectedPackage/version | jq -r ".[].game_versions[]" | grep $ServerVersion
+}
+
+function PackageExists {
+    ls $ServerRoot/mods | grep -E "^${1}_[^_]+\.jar$" || echo "NONEXISTENT"
+}
+
+function GetPackageInformation {
+    if [[ $SelectedPackage == "" ]]; then
+        LogFail "Please specify a package."
+    fi
+    curl https://api.modrinth.com/v2/project/$SelectedPackage | jq -r | less
+}
+
+function GetPackageVersions {
+    if [[ $SelectedPackage == "" ]]; then
+        LogFail "Please specify a package."
+    fi
+    curl https://api.modrinth.com/v2/project/sodium/version | jq -r | less
+}
+
+function IsRunningInServer {
+    test -f $ServerRoot/cpm.conf
+}
+
+Log() {
+    case $1 in
+        s)
+            lmsg_one="STAGE"
+        ;;
+        i)
+            lmsg_one="Installing package"
+        ;;
+        r)
+            lmsg_one="Removing package"
+        ;;
+    esac
+    lmsg_two=$2
+    case $3 in
+        i)
+            lmsg_three="${txtblu}INFO"
+        ;;
+        w)
+            lmsg_three="${txtylw}WARN"
+        ;;
+        e)            
+            lmsg_three="${txtred}ERRR"
+        ;;
+        *)
+            lmsg_three=$3
+        ;;
+    esac
+    echo -e "[ChiselPM/${txtcyn}${lmsg_one} ${lmsg_two}${txtreg}/${lmsg_three}${txtreg}]: $4"
+}
+
+LogFail() { 
+    echo -e "${txtred}Error:${txtreg} ${1}"; exit 1 
+}
+
+Main "$@"
